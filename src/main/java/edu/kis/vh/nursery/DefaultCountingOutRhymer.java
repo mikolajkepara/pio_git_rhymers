0ae426bb2ccb8930e758c9cb2349421a3f342b86
Olyaokh <258486@edu.p.lodz.pl>
2026-03-26 23:06:03 +0200

    Fix constants in DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,41 +2,42 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private static final int size_array = 12;
-    private static final int num2 = -1;
-    private static final int num3 = 11;
+    private static final int CAPACITY = 12;
+    private static final int EMPTY_INDEX = -1;
+    private static final int LAST_INDEX = CAPACITY - 1;
 
-    private final int[] numbers = new int[size_array];
-
-    private int total = num2;
+    private final int[] numbers = new int[CAPACITY];
+    private int total = EMPTY_INDEX;
 
     public int getTotal() {
         return total;
     }
 
     public void countIn(int in) {
-        if (!isFull())
+        if (!isFull()) {
             numbers[++total] = in;
+        }
     }
 
     public boolean callCheck() {
-        return total == num2;
+        return total == EMPTY_INDEX;
     }
 
     public boolean isFull() {
-        return total == num3;
+        return total == LAST_INDEX;
     }
 
     protected int peekaboo() {
-        if (callCheck())
-            return num2;
+        if (callCheck()) {
+            return EMPTY_INDEX;
+        }
         return numbers[total];
     }
 
     public int countOut() {
-        if (callCheck())
-            return num2;
+        if (callCheck()) {
+            return EMPTY_INDEX;
+        }
         return numbers[total--];
     }
-
 }
